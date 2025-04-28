e5c5d447ece177aecbb9488473af10560d09598a
ming-12321 <thdalstj0450@gmail.com>
2025-04-28 19:13:48 +0900

    loginPage

--- src/main/java/com/hulzzuk/user/controller/UserController.java
+++ src/main/java/com/hulzzuk/user/controller/UserController.java
@@ -3,14 +3,19 @@ package com.hulzzuk.user.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hulzzuk.user.model.service.UserService;
+import com.hulzzuk.user.model.vo.UserVO;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("user")
@@ -20,10 +25,45 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	// 내 정보 보기 페이지
-	@RequestMapping(value = "select.do", method = RequestMethod.GET, produces = "application/json; charset:UTF-8")
-	public ModelAndView userDetailMethod(@RequestParam("userId") String userId) {
-		
-	    return userService.selectUser(userId);
+	// 마이 페이지
+	@RequestMapping(value = "select.do", method = RequestMethod.GET)
+	public ModelAndView userDetailMethod(ModelAndView mv, @RequestParam("userId") String userId) {
+	    return userService.selectUser(mv, userId);
 	}
+	
+	// 로그인 페이지 이동
+	@RequestMapping(value = "login.do", method = RequestMethod.GET) 
+	public ModelAndView moveLoginPage(ModelAndView mv) {
+		
+		mv.addObject("fail", "N");
+		mv.setViewName("user/loginPage");
+		
+		return mv; 
+	}
+	
+	// 로그인 처리
+	@RequestMapping(value = "login.do", method = RequestMethod.POST)
+	public ModelAndView loginMethod(ModelAndView mv, UserVO user, HttpSession session, SessionStatus status) {
+		return userService.loginMethod(mv, user, session, status);
+	}
+	
+	// 로그아웃 처리
+	@RequestMapping(value = "logout.do", method = RequestMethod.GET)
+	public ModelAndView logoutMethod(ModelAndView mv, HttpSession session, SessionStatus status) {
+		return userService.logoutMethod(mv, session, status);
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/hulzzuk/user/model/service/UserService.java
+++ src/main/java/com/hulzzuk/user/model/service/UserService.java
@@ -1,7 +1,20 @@
 package com.hulzzuk.user.model.service;
 
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hulzzuk.user.model.vo.UserVO;
+
+import jakarta.servlet.http.HttpSession;
+
 public interface UserService {
-	ModelAndView selectUser(String userId);
+	// 내 정보 보기
+	ModelAndView selectUser(ModelAndView mv, String userId);
+	
+	// 로그인
+	ModelAndView loginMethod(ModelAndView mv, UserVO user, HttpSession session, SessionStatus status);
+	
+	// 로그아웃
+	ModelAndView logoutMethod(ModelAndView mv, HttpSession session, SessionStatus status);
+	
 }
